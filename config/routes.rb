bc60a5fe81e4f55ab7ab6e696c7dd2edc93147a7
FaheemAhmedNoor <faheemahmednoor12@gmail.com>
2022-06-01 02:05:36 +0500

    Add an Article Controller and Model

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+  root 'articles#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
